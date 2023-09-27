9b783946f3a7743695c28792766d841da4816e60
Mizuinu30 <juansilva.dvm@gmail.com>
2023-09-27 14:47:51 +0000

    backwards

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def print_tebalhpla():
-    for char_code in range(ord("z"), ord("A") - 1, -1):
+for char_code in range(ord("z"), ord("A") - 1, -1):
         char = chr(char_code)
         print(char, end="")

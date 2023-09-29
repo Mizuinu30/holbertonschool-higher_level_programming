e6c897c559bccca84ce2693c2783320936c1a3bf
Mizuinu30 <juansilva.dvm@gmail.com>
2023-09-29 12:50:29 +0000

    fixed typo and indentation

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from aadd_0 import add
+from add_0 import add
 
 if __name__ == "__main__":
-a = 1
-b = 2
-
-print("{} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

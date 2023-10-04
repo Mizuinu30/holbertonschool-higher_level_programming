3cfba74b29d41ca353fd3219d3536d10bc5ed20c
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-04 15:25:45 -0400

    changed to include for loop

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,2 +1,7 @@
+#!/usr/bin/python3
 def no_c(my_string):
-    return ''.join([char for char in my_string if char.lower() != 'c'])
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_string += my_string[i]
+    return new_string

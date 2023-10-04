b894cfa43bdd9b51f81008ea833f3795b80c291f
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-04 14:54:20 -0400

    added to verify if there;s no list.

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # A function that prints all items in a list in reverse order
 def print_reversed_list_integer(my_list=[]):
-    # With these parameters, the loop iterates in reverse order, starting from the last element of the list and moving towards the first element, one step at a time.
+    if my_list is None:
+        return
+    # With these parameters, the loop iterates in reverse order.
     for i in range(len(my_list) - 1, -1, -1):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))

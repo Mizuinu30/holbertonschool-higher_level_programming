4d732e421cfca8c0c47d7004c6b35761d5cccfa7
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-11 11:23:25 -0400

    a class that difines a square

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
-        result = 0
+        quotient = 0
         try:
-            result = my_list_1[i] / my_list_2[i]
+            quotient = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
         except ZeroDivisionError:
@@ -13,5 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            new_list.append(result)
+            new_list.append(quotient)
     return new_list

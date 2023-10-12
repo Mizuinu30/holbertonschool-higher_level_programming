8015a5ffdfad92f6ca4ff0404e5c8a5337077ed1
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-12 18:55:32 -0400

    Add Say_my_name and text file with corresponding edgecases

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -17,5 +17,5 @@ def say_my_name(first_name, last_name=""):
     """ print my first and last name """
     str_error = "first_name must be a string or last_name must be a string"
     if type(first_name) is not str or type(last_name) is not str:
-        raise TypeError(str_error)
+        raise TypeError(str_error, "Anything")
     print("My name is", first_name, last_name)

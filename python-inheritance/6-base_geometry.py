8911c8d95c3b67e18a0f81b5cb50e1a7b95e3d87
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-19 12:09:23 -0400

    Add a class based on 5-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module to create a class based on 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Class that defines a base geometry"""
+
+    def area(self):
+        """Method that raises an exception"""
+        raise Exception("area() is not implemented")

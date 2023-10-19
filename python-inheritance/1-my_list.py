e6ffc0b846616e37dc54e94429ceeeebf3209940
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-19 10:40:08 -0400

    Add edge cases

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" Mylist Class
-    Module that defines a class Mylist that inherits from list"""
+"""
+    Module that defines a class Mylist that inherits from list
+"""
 
 
 class MyList(list):
     """class that inherits from list"""
 
     def print_sorted(self):
-        """prints the list, but sorted (ascending sort)"""
+        """
+        prints the list, but sorted (ascending sort)
+        """
         list_sorted = []
         for num in self:
             list_sorted.append(num)

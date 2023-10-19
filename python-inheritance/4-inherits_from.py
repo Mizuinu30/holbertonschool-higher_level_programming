f8fb26f41967d423035fc83411f7017943343dbc
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-19 11:21:22 -0400

    Add a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class; otherwise False.

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a function that returns True if the object is an
+instance of, or if the object is an instance of a class that inherited from,
+the specified class; otherwise False."""
+
+
+def inherits_from(obj, a_class):
+    """checks if object is instance of a class that inherited from a_class
+
+    Args:
+        obj: object
+        a_class: class
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False

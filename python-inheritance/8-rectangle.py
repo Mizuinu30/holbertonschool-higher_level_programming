57689be662120faceb6674c2ceb222d2b802b8e9
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-19 13:14:18 -0400

    Add a class base on 7-base_geometry,py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Module to create a class based on 7-base_geometry.py"""
-
-Basegemetry = __import__('7-base_geometry').BaseGeometry
+"""Write a class Rectangle that inherits from BaseGeometry"""
 
 
-class Rectangle(Basegemetry):
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """rectangle that inherits from BaseGeometry
     """
-        rectangle taht inherits from BaseGeometry
-    """
-    def__init__(self, width, height):
+
+    def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
+        self.__height = height
         self.__height = height

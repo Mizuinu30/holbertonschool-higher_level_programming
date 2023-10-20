7e8c43282fed75c76918f2ded49ca007706d3276
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-20 16:31:08 -0400

    Add a class Student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" defines a class Student"""
+
+
+class Student:
+    """defines a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance
+        Raises:
+        list of strings, only attributes names must be retrieved
+        else all attributes must be retrieved
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict

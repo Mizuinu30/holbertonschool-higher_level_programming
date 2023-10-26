1706ecf2aa8d75e8ba32323127701a29b0486124
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-26 15:31:56 -0400

    Add documentation

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -10,70 +10,89 @@ class TestBase_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Base class."""
 
     def test_no_arg(self):
+        """ Test instantiation with no arguments. """
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, b2.id - 1)
 
     def test_three_bases(self):
+        """ Test instantiation of three Base instances.
+        """
         b1 = Base()
         b2 = Base()
         b3 = Base()
         self.assertEqual(b1.id, b3.id - 2)
 
     def test_None_id(self):
+        """ Test instantiation with id as None."""
         b1 = Base(None)
         b2 = Base(None)
         self.assertEqual(b1.id, b2.id - 1)
 
     def test_unique_id(self):
+        """ Test instantiation with unique id."""
         self.assertEqual(12, Base(12).id)
 
     def test_nb_instances_after_unique_id(self):
+        """ Test instantiation with unique id"""
         b1 = Base()
         b2 = Base(12)
         b3 = Base()
         self.assertEqual(b1.id, b3.id - 1)
 
     def test_id_public(self):
+        """ Test if id is public."""
         b = Base(12)
         b.id = 15
         self.assertEqual(15, b.id)
 
     def test_nb_instances_private(self):
+        """ Test if nb_instances is private."""
         with self.assertRaises(AttributeError):
             print(Base(12).__nb_instances)
 
     def test_str_id(self):
+        """ Test instantiation with string id."""
         self.assertEqual("hello", Base("hello").id)
 
     def test_float_id(self):
+        """ Test instantiation with float id."""
         self.assertEqual(5.5, Base(5.5).id)
 
     def test_complex_id(self):
+        """ Test instantiation with complex id."""
         self.assertEqual(complex(5), Base(complex(5)).id)
 
     def test_dict_id(self):
+        """ Test instantiation with dict id."""
         self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
 
     def test_bool_id(self):
+        """ Test instantiation with bool id."""
         self.assertEqual(True, Base(True).id)
 
     def test_list_id(self):
+        """ Test instantiation with list id."""
         self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
 
     def test_tuple_id(self):
+        """ Test instantiation with tuple id."""
         self.assertEqual((1, 2), Base((1, 2)).id)
 
     def test_set_id(self):
+        """ Test instantiation with set id."""
         self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
 
     def test_frozenset_id(self):
+        """ Test instantiation with frozenset id."""
         self.assertEqual(frozenset({1, 2, 3}), Base(frozenset({1, 2, 3})).id)
 
     def test_range_id(self):
+        """ Test instantiation with range id."""
         self.assertEqual(range(5), Base(range(5)).id)
 
     def test_bytes_id(self):
+        """ Test instantiation with bytes id."""
         self.assertEqual(b'Python', Base(b'Python').id)
 
     def test_bytearray_id(self):
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -7,52 +7,62 @@ class TestSquare(unittest.TestCase):
     """ Checker preference """
 
     def test_checker(self):
+        """ Test checker """
         with self.assertRaises(ValueError):
             sqr = Square(0)
 
     """ Testing size """
 
     def test_size(self):
+        """ Test size """
         sqr = Square(1, 2)
         self.assertEqual(sqr.size, 1)
 
     """ Testing width """
 
     def test_width(self):
+        """ Test width """
         sqr = Square(1, 2, 3)
         self.assertEqual(sqr.width, 1)
 
     """ Testing a str as argument """
 
     def test_str_1_arg(self):
+        """ Test str"""
         with self.assertRaises(TypeError):
             sqr = Square("1")
 
     def test_str_2_arg(self):
+        """ Test str"""
         with self.assertRaises(TypeError):
             sqr = Square(1, "2")
 
     def test_str_3_arg(self):
+        """ Test str"""
         with self.assertRaises(TypeError):
             sqr = Square(1, 2, "3")
 
     """ Testing Negative Arguments """
 
     def test_neg_arg_1(self):
+        """ Test negative argument"""
         with self.assertRaises(ValueError):
             sqr = Square(-1)
 
     def test_neg_arg_2(self):
+        """ test negative argument"""
         with self.assertRaises(ValueError):
             sqr = Square(1, -2)
 
     def test_neg_arg_3(self):
+        """ test negative argument"""
         with self.assertRaises(ValueError):
             sqr = Square(1, 2, -3)
 
     """ Testing to_dictionary method """
 
     def test_to_dic(self):
+        """ test to dictionary"""
         sqr = Square(10, 2, 1, 6)
         result = {'id': 6, 'x': 2, 'size': 10, 'y': 1}
         self.assertDictEqual(sqr.to_dictionary(), result)
@@ -60,6 +70,7 @@ class TestSquare(unittest.TestCase):
     """ Testing update method """
 
     def test_update(self):
+        """ Test update method"""
         sqr = Square(5)
         sqr.update(10)
         self.assertEqual(sqr.id, 10)
@@ -74,6 +85,7 @@ class TestSquare(unittest.TestCase):
     """ Testing create method """
 
     def test_create(self):
+        """ Test create method """
         sqr1 = Square(2)
         sqr1_dic = sqr1.to_dictionary()
         sqr2 = Square.create(**sqr1_dic)
@@ -82,11 +94,13 @@ class TestSquare(unittest.TestCase):
     """ Testing to save to file """
 
     def test_save_to_file_empty(self):
+        """ Test save to file empty"""
         Square.save_to_file(None)
         with open("Square.json") as fd:
             self.assertEqual('[]', fd.read())
 
     def test_save_to_file_empty_2(self):
+        """ Test save to file empty"""
         Square.save_to_file([])
         with open("Square.json") as fd:
             self.assertEqual('[]', fd.read())
@@ -94,6 +108,7 @@ class TestSquare(unittest.TestCase):
     """ Test load from file """
 
     def test_load_file(self):
+        """ test load from file"""
         s1 = Square(5)
         s2 = Square(7, 9, 1)
         list_squares_input = [s1, s2]

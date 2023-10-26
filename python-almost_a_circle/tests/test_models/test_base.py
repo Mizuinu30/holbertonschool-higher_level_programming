158e3fd007bb350a4513694532039e09e0ba980c
Mizuinu30 <juansilva.dvm@gmail.com>
2023-10-26 09:59:08 -0400

    Added test for main 0

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,23 @@ from models.base import Base
 from models.rectangle import Rectangle
 import unittest
 
+if __name__ == "__main__":
+
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
+
 
 class TestBase(unittest.TestCase):
     """ Test for initialization """

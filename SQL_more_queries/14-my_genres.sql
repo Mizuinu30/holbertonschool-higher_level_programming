e0bff21d95c6fc6b107801b0aacf5de06f5a4bc7
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-21 22:11:04 -0400

    special

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,9 @@
--- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT
-    tv_genres.name AS genre,
-    COUNT(show_id) AS number_of_shows
+--a script that lists all the genres.
 FROM
     tv_genres
     JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY
-    genre
+    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE
+    title = 'Dexter'
 ORDER BY
-    number_of_shows DESC;
+    tv_genres.name;

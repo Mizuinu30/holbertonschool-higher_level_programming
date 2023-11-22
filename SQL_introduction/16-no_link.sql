2e064340a217c693d3d0387e037d5bbd513d347f
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-21 20:18:58 -0400

    Not equal

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,10 +1,10 @@
--- script that lists all records of the table second_table of the database.
+-- Lists rows with name value
+-- Execute: cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT
     score,
     name
 FROM
-    second_table;
-
+    second_table
 WHERE
     name != ''
     OR name IS NOT NULL

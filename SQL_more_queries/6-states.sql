4e4e8e3b6ef127cd7d4ed44beb94f0ef0d1dceee
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-21 21:29:01 -0400

    usa usa

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates a database "hbtn_od_usa" and a table "states".
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE (id)
+)

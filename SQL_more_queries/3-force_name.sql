79c4f60030d3fd3373329e9f5c9cbd713445ccf5
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-21 21:05:50 -0400

    May it be with you

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- a script that creates the table
-CREATE TABLE If not exists 'Foce_name' ('id' INT, 'name' VARCHAR(256) NOT NULL);
+CREATE TABLE If not exists 'Foce_name' (id INT, name VARCHAR(256) NOT NULL);

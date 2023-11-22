31898f605f2ef19f07da4a5a765688a3fed64ea1
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-21 21:06:54 -0400

    May it be with you

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,3 @@
--- a script that creates the table
-CREATE TABLE If not exists 'Foce_name' (id INT, name VARCHAR(256) NOT NULL);
+-- Create a table
+-- Execute: cat 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+CREATE TABLE IF NOT EXISTS `force_name` (id INT, name VARCHAR(256) NOT NULL);

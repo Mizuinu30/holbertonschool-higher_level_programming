21e5782e03b0c39ecff5593369c7142fad3e99cd
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-21 20:10:24 -0400

    Added the As

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,7 +1,5 @@
---a script that computes the score average of all records in the table
+-- Print the average score
 SELECT
-    AVG(score) AS average_score
+    AVG(score) AS average
 FROM
     second_table;
-
-```

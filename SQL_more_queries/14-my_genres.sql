ffa39d1c982b949c7bd8ab679fd25794cb555894
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-21 22:13:00 -0400

    special

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,11 @@
 --a script that lists all the genres.
+SELECT
+    tv_genres.name
 FROM
     tv_genres
     JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE
-    title = 'Dexter'
+    tv_shows.title = 'Dexter'
 ORDER BY
-    tv_genres.name;
+    tv_genres.name ASC;

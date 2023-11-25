b253a6dc2fd84234adf06eb03fe63f8bb885d63e
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-25 07:40:25 -0400

    hot fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
-
-import MySQLdb
-from sys import argv
-
-if __name__ == "__main__":
-
-    """Connect to a MySQL server."""
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-
-    """Create a cursor object."""
+""" This script return all states """
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
-
-    """Execute the query."""
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    """Print the results of the query."""
-    for row in cur.fetchall():
+    cur.execute("""SELECT * FROM states ORDER BY id""")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    """Close all cursors."""
     cur.close()
-
-    """Close all databases."""
     db.close()

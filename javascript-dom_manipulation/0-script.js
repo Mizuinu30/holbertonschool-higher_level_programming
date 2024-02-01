101ade44329dce4c3dac2b990ca512055f677f7b
Mizuinu30 <juansilva.dvm@gmail.com>
2024-02-01 16:09:56 -0400

    Add a script that updates the text color

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,6 @@
+// Select the header element using document.querySelector
+const header = document.querySelector('header');
+
+// Set the color of the text to red (#FF0000)
+header.style.color = '#FF0000';
+
